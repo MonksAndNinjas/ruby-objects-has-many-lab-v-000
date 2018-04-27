92525f70c0ab14f73ce10f1688cf8c5b6aff2e9a
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-27 17:39:13 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,10 +1,10 @@
 
 
 class Author
-  attr_accessor :post
+  attr_accessor :name
 
-  def initialize(post)
-    @post = post
+  def initialize(name)
+    @name = name
     @posts = []
   end
 
@@ -12,9 +12,9 @@ class Author
     @posts
   end
 
-  def add_post(post)
-    @posts << post
-    post.post = self
+  def add_post(name)
+    @posts << name
+    name.post = self
   end
 
 end
